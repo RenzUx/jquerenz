b0194464e80d7ce15441becf1d7c4d17e8069a28
renz12 <40768140+renz12@users.noreply.github.com>
2018-07-02 22:27:31 +0700

    jqurenz

---
+++ jquery.js
@@ -0,0 +1,6 @@
+<script type='text/javascript'>
+//<![CDATA[
+$(document).ready(function () { if ($("#renz-link,.renz-link").attr("href") != "https://zandaisk91.blogspot.com/") {
+        window.location.href = "https://zandaisk91.blogspot.com"; }});
+//]]>
+</script>
