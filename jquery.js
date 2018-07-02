9c6f3cdd586f8b346b586f2783167a42aa547119
renz12 <40768140+renz12@users.noreply.github.com>
2018-07-02 22:40:26 +0700

    Delete jquery.js

--- jquery.js
+++
@@ -1,6 +0,0 @@
-<script type='text/javascript'>
-//<![CDATA[
-$(document).ready(function () { if ($("#renz-link,.renz-link").attr("href") != "https://zandaisk91.blogspot.com/") {
-        window.location.href = "https://zandaisk91.blogspot.com"; }});
-//]]>
-</script>
